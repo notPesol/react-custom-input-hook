a5e21fa810b7a8809ffb1d597cefe1a5ef1aa51e
notPesol <pesol2012@hotmail.com>
2022-06-11 11:44:46 +0700

    save custom-hook with useReducer

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,29 +1,61 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 const defaultValidateValue = (value) => value.trim() !== "";
 
-const useInput = (validateValue = defaultValidateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouch] = useState(false);
+const inputStateReducer = (prevState, action) => {
+  if (action.type === "CHANGE") {
+    return {
+      ...prevState,
+      value: action.value,
+    };
+  } else if (action.type === "BLUR") {
+    return {
+      ...prevState,
+      isTouch: true,
+    };
+  } else if (action.type === "RESET") {
+    return {
+      value: "",
+      isTouch: false,
+    };
+  }
+  // default bahavior (return prev state)
+  return prevState;
+};
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+const useInput = (validateValue = defaultValidateValue) => {
+  const [state, dispathState] = useReducer(inputStateReducer, {
+    value: "",
+    isTouch: false,
+  });
+
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [isTouched, setIsTouch] = useState(false);
+
+  // const valueIsValid = validateValue(enteredValue);
+  // const hasError = !valueIsValid && isTouched;
+
+  const valueIsValid = validateValue(state.value);
+  const hasError = !valueIsValid && state.isTouch;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    // setEnteredValue(event.target.value);
+    dispathState({ type: "CHANGE", value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouch(true);
+    // setIsTouch(true);
+    dispathState({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouch(false);
+    // setEnteredValue("");
+    // setIsTouch(false);
+    dispathState({ type: "RESET" });
   };
 
   return {
-    value: enteredValue,
+    value: state.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
